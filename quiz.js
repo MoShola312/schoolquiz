56a8b8125d23f6950409691367277ae25a0696c8
Mohammed S <37146251+MoShola312@users.noreply.github.com>
2019-03-22 08:55:55 -0500

    reset button was added

--- quiz.js
+++ quiz.js
@@ -29,17 +29,16 @@ var test = [
 
 }
 ]
-  var currentQuestion = 0;
-  var score = 0;
+var currentQuestion = 0;
+var score = 0;
+var button = document.querySelector("#button")
+var reset = document.querySelector("#reset");
+var input = document.getElementsByName('t');
 
+//hides the reset button
+reset.style.visibility = "hidden"
 
-  var button = document.querySelector("#button");
-  var input = document.getElementsByName('t');
-
-
-  //get the value of the selected button
-
-
+//get the value of the selected button
 button.addEventListener("click", function(){
 for(var i=0; i<input.length; i++){
     //check to see what radio button is selected
@@ -64,23 +63,24 @@ for(var i=0; i<input.length; i++){
    input[i].checked = false;
 
    //ENDS THE GAME
+   //if a question is added to the array, increase the number to 4
    if(currentQuestion > 3){
      button.style.visibility = "hidden";
-     document.querySelector("#yourScore").innerHTML=("Game over! Your total is " + score)
+     document.querySelector("#yourScore").innerHTML=("Game over! Your total is " + score);
+     reset.style.visibility = "visible";
      return;
    }
    //update to the next question
    loadQuestion(currentQuestion);
-
   }
 }
 console.log("score " + score)
 console.log("currentQuestion " + currentQuestion)
-
-})
+});
 //load the first question
 loadQuestion(currentQuestion);
 
+//loads the question
 function loadQuestion(currentQuestion){
   document.querySelector("#questionTitle").textContent = test[currentQuestion].question
   document.querySelector("#Q1").setAttribute("value", test[currentQuestion].option1)
@@ -89,5 +89,14 @@ function loadQuestion(currentQuestion){
  document.querySelector("#two").textContent = test[currentQuestion].option2
  document.querySelector("#Q3").setAttribute("value", test[currentQuestion].option3)
  document.querySelector("#three").textContent = test[currentQuestion].option3
+};
 
-}
+//button for reset, rests the game
+reset.addEventListener("click", function(){
+  currentQuestion = 0;
+  score = 0;
+  document.querySelector("#yourScore").innerHTML=(" ");
+  loadQuestion(currentQuestion);
+  reset.style.visibility = "hidden";
+  button.style.visibility = "visible";
+});
